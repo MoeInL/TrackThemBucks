560ebfaa531ba14e12e8f39a5e1476857650931d
unknown <mohamad.alameh132000@gmail.com>
2023-08-10 22:54:54 +0300

    navigation was setup

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer} from '@react-navigation/native'
+
+import OnBoarding1 from './Screens/OnBoarding/OnBoarding1';
+import OnBoarding2 from './Screens/OnBoarding/OnBoarding2';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+
+      <NavigationContainer>
+
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="OB1" component={OnBoarding1} />
+          <Stack.Screen name="OB2" component={OnBoarding2} />
+        </Stack.Navigator>
+
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
