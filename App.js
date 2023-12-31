dc31b56729968f706ee59402de736ce23bed35a2
unknown <mohamad.alameh132000@gmail.com>
2023-12-31 16:36:40 +0200

    Restarting App

--- App.js
+++ App.js
@@ -3,9 +3,6 @@ import { StatusBar } from 'expo-status-bar';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer} from '@react-navigation/native'
 
-import OnBoarding1 from './Screens/OnBoarding/OnBoarding1';
-import OnBoarding2 from './Screens/OnBoarding/OnBoarding2';
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -15,9 +12,8 @@ export default function App() {
 
       <NavigationContainer>
 
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="OB1" component={OnBoarding1} />
-          <Stack.Screen name="OB2" component={OnBoarding2} />
+        <Stack.Navigator>
+         
         </Stack.Navigator>
 
       </NavigationContainer>
