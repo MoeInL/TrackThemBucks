bc3ff812a5fe91b48129cc218cf159a909c35721
unknown <mohamad.alameh132000@gmail.com>
2024-01-06 17:56:33 +0200

    renamed the UI files to .jsx

--- States/Store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import UserInfoSlice from './UserInfoSlice';
-
-export const store = configureStore({
-    reducer: {
-        userInfoData: UserInfoSlice,
-    }
-})
